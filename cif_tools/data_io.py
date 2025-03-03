42eb0e1cef38caa7f5c4a42d3c11f45a8e20431b
Wheeler1711 <Wheeler1711@gmail.com>
2025-03-03 09:48:42 -0700

    try to preserve indexes in file when loading

--- cif_tools/data_io.py
+++ cif_tools/data_io.py
@@ -6,6 +6,7 @@ def load_cif(load_filename,index_dict,working_file):
     '''
     load file is the file you want to copy over into your working_file
     must have an index_dict for that working_file
+    you can have every index increased by the index_dict.index_offset
     '''
     with open(load_filename, "r") as f:
         while line := f.readline():
@@ -18,7 +19,9 @@ def load_cif(load_filename,index_dict,working_file):
                     old_index = int(split_line[1])
                     scale_num = int(split_line[2])
                     scale_denom = int(split_line[3].removesuffix(";\n"))
-                    working_file.write(line)
+                    #working_file.write(line)
+                    new_line = split_line[0]+" "+str(old_index+index_dict.index_offset)+" "+split_line[2]+" "+split_line[3]
+                    working_file.write(new_line)
                     # next line
                     line = f.readline()
                     split_line = line.split(" ")
@@ -26,7 +29,8 @@ def load_cif(load_filename,index_dict,working_file):
                         name = split_line[1].removesuffix(";\n")
                     else: #no name     will name always be second?
                         name = "imported_from_"+load_filename[0:-3]+"index_"+str(old_index)
-                    index_dict.add_new_index(name)
+                    #index_dict.add_new_index(name) # this is a bit tricky we don't want to screw up the indexes of the loaded file
+                    index_dict.id[name] = old_index +index_dict.index_offset
                     working_file.write('9 '+name+';\n')
                     while not end:
                         line = f.readline()
